962d2ddf3ee5662c661e223c368fc2d4fe938bab
soulaimane007 <soulaymane.cherr@gmail.com>
2026-08-07 21:12:13 +0100

    api url fix

--- frontend/invoice-frontend/src/api/client.js
+++ frontend/invoice-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://169.58.137.92:8000/api",
 });
 
 apiClient.interceptors.request.use((config) => {
